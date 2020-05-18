df1cd83b9d3449e756c0b3eb014500b9a34fadc1
olaf <arielolafsalgado@gmail.com>
2020-05-18 18:33:48 -0300

    Erased tilde in departamentos

--- geoche_extras/trabajo/paso2.R
+++ geoche_extras/trabajo/paso2.R
@@ -1,5 +1,5 @@
 require(geoche)
 inputArchivo = 'bases/ejemplo/ejemplo.csv'
-departamentos_path = 'shp/departamentos/censo_por_departamento_polígonos.shp'
+departamentos_path = 'shp/departamentos/censo_por_departamento_poligonos.shp'
 depto_id_column='ID_DEPTO_INDEC_RESIDENCIA'
 output = checkea_departamentos_ggmap_y_osm(inputArchivo,departamentos_path,depto_id_column)
